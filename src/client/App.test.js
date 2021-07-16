fa25b4f85ccbbd7f8ccaa039c83e10475547a233
Sasha Chan <>
2021-07-16 10:56:14 +0100

    update app and add proxy

--- src/client/App.test.js
+++ src/client/App.test.js
@@ -1,11 +1,24 @@
-import React from 'react'
-import { render, screen } from '@testing-library/react'
-import App from './App'
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import App from "./App";
+import { AppContext } from "./contexts/app.context";
+
+global.fetch = jest
+  .fn()
+  .mockImplementation(() =>
+    Promise.resolve({ json: () => Promise.resolve({ items: [] }) })
+  );
 
 // Example test
-describe('App tests', () => {
-    it('renders a message', () => {
-        render(<App />)
-        expect(screen.getByText('6 items')).toBeInTheDocument()
-    })
-})
+describe("App tests", () => {
+  it("renders a message", () => {
+    render(
+      <AppContext.Provider
+        value={{ subMenuItems: [], subMenuDispatch: jest.fn() }}
+      >
+        <App />
+      </AppContext.Provider>
+    );
+    expect(screen.getByText("0 items")).toBeInTheDocument();
+  });
+});
